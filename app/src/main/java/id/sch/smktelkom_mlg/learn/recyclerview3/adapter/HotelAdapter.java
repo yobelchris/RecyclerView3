4c78ffdd5934f5cd5fa1784317e4424eece8806f
yobelchris <yobelchris@gmail.com>
2017-01-19 22:38:20 +0700

    RecyclerView List Card

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/adapter/HotelAdapter.java
@@ -0,0 +1,63 @@
+package id.sch.smktelkom_mlg.learn.recyclerview3.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import id.sch.smktelkom_mlg.learn.recyclerview3.R;
+import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
+
+/**
+ * Created by rex on 02/11/2016.
+ */
+
+public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> {
+
+    ArrayList<Hotel> hotelist;
+
+    public HotelAdapter(ArrayList<Hotel> hotelist) {
+        this.hotelist = hotelist;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Hotel hotel = hotelist.get(position);
+        holder.tvJudul.setText(hotel.judul);
+        holder.tvDeskripsi.setText(hotel.deskripsi);
+        holder.ivFoto.setImageDrawable(hotel.foto);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (hotelist != null) {
+            return hotelist.size();
+        }
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView ivFoto;
+        TextView tvJudul;
+        TextView tvDeskripsi;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ivFoto = (ImageView) itemView.findViewById(R.id.imageView);
+            tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
+            tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
+        }
+    }
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
@@ -0,0 +1,19 @@
+package id.sch.smktelkom_mlg.learn.recyclerview3.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by rex on 01/11/2016.
+ */
+
+public class Hotel {
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public Hotel(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
